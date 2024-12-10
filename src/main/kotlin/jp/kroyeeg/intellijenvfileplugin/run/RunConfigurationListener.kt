da2e83b4e8be06a48646855b1f43c2a46d24873d
ogawa.shogo <ogawa.shogo@moneyforward.co.jp>
2024-12-10 10:38:13 +0900

    Enhance environment variable handling in run configs
    Updated the RunConfigurationListener to better handle environment variables by factoring them through a centralized method upon addition and modification. The refactored code now ensures that the expanded environment variables are correctly assigned to run configurations, improving maintainability and reducing redundancy.

--- src/main/kotlin/jp/kroyeeg/intellijenvfileplugin/run/RunConfigurationListener.kt
+++ src/main/kotlin/jp/kroyeeg/intellijenvfileplugin/run/RunConfigurationListener.kt
@@ -8,19 +8,29 @@ import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunCo
 class RunConfigurationListener : RunManagerListener {
 
     override fun runConfigurationAdded(settings: RunnerAndConfigurationSettings) {
+        updateEnvironmentVariables(settings)
+        super.runConfigurationAdded(settings)
+    }
+
+    override fun runConfigurationChanged(settings: RunnerAndConfigurationSettings) {
+        updateEnvironmentVariables(settings)
+        super.runConfigurationChanged(settings)
+    }
+
+    private fun updateEnvironmentVariables(settings: RunnerAndConfigurationSettings) {
         val env = DotEnvFilePluginService.getInstance(settings.configuration.project)
             .state.variables.associate { it.key to it.value }
         val expandedEnv = expandEnvironmentVariables(env)
         settings.configuration.let { config ->
             if (config is ExternalSystemRunConfiguration) {
                 val mySettings = config.settings
-                mySettings.env.putAll(expandedEnv)
+                val newEnv = mySettings.env + expandedEnv
+                mySettings.env = newEnv
             }
         }
-        super.runConfigurationAdded(settings)
     }
 
-    private fun expandEnvironmentVariables(env: Map<String, String>): Map<String, String> {
+    private fun expandEnvironmentVariables(env: Map<String, String>): MutableMap<String, String> {
         val regex = """\$\{([^}]+)}""".toRegex()
 
         fun resolve(value: String, seenKeys: Set<String> = emptySet()): String {
@@ -35,18 +45,10 @@ class RunConfigurationListener : RunManagerListener {
             }
         }
 
-//        val result = mutableMapOf<String, String>()
-//        env.forEach { (key, value) ->
-//            result[key] = regex.replace(value) { matchResult ->
-//                val replaceKey = matchResult.groups[1]?.value ?: return@replace matchResult.value
-//                env[replaceKey] ?: matchResult.value
-//            }
-//        }
-//        return result.toMap()
         val result = mutableMapOf<String, String>()
         env.forEach { (key, value) ->
             result[key] = resolve(value)
         }
-        return result.toMap()
+        return result
     }
 }
